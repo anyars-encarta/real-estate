7a5fd075fb712fcd4c987e20151736554a2472d7
anyars-encarta <anyarsencarta@gmail.com>
2024-04-01 17:18:07 +0000

    Implement list section

--- src/pages/listPage/List.jsx
+++ src/pages/listPage/List.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
 import './list.scss';
+import { listData } from '../../lib/dummyData';
+import Filter from '../../components/filter/Filter';
+import Card from '../../components/card/Card';
 
 const List = () => {
+  const data = listData;
+
   return (
-    <div>List</div>
+    <div className='listPage'>
+      <div className="listContainer">
+        <Filter />
+        <div className="wrapper">
+
+          {
+            data.map((dataItem) => (
+              <Card key={dataItem.id} item={dataItem} />
+            ))
+          }
+        </div>
+      </div>
+
+      <div className="mapContainer">Map</div>
+    </div>
   )
 }
 
