4ede49a969ebb49a1d3d3e2e9ce7e134ea0db4e3
anyars-encarta <anyarsencarta@gmail.com>
2024-04-01 11:07:40 +0000

    Implement Navigations

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
+import { createBrowserRouter, RouterProvider, } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
-import './layout.scss';
 import Home from './pages/homePage/Home';
+import List from './pages/listPage/List';
+import Layout from './pages/layout/Layout';
+import SinglePage from './pages/singlePage/SinglePage';
 
 const App = () => {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <Home />
+        },
+        {
+          path: '/list',
+          element: <List />
+        },
+        {
+          path: '/:id',
+          element: <SinglePage />
+        }
+      ]
+    },
+  ]);
+
   return (
-    <div className='layout'>
-      <div className="navbar">
-        <Navbar />
-      </div>
-      
-      <div className="content">
-        <Home />
-      </div>
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
