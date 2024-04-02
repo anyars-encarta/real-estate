76d0b2fe78f8d82d065ee3d55057a8d6723ee03d
anyars-encarta <anyarsencarta@gmail.com>
2024-04-02 08:56:35 +0000

    Implementing left section of single page

---
+++ src/components/slider/Slider.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './slider.scss';
+
+const Slider = ({ images }) => {
+  return (
+    <div className='slider'>Slider</div>
+  )
+}
+
+export default Slider
--- src/pages/singlePage/SinglePage.jsx
+++ src/pages/singlePage/SinglePage.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
 import './singlePage.scss';
+import Slider from '../../components/slider/Slider';
+import { singlePostData, userData } from '../../lib/dummyData';
 
 const SinglePage = () => {
   return (
-    <div>SinglePage</div>
+    <div className='singlePage'>
+      <div className="details">
+        <div className="wrapper">
+          <Slider images={singlePostData.images} />
+          <div className="info">
+            <div className="top">
+              <div className="post">
+                <h1>{singlePostData.title}</h1>
+                <div className="address">
+                  <img src="/pin.png" alt="" />
+                  <span>{singlePostData.address}</span>
+                </div>
+                <div className="price">$ {singlePostData.price}</div>
+              </div>
+              <div className="user">
+                <img src={userData.img} alt="" />
+                <span>{userData.name}</span>
+              </div>
+            </div>
+            <div className="bottom">
+              {singlePostData.description}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="features">
+        <div className="wrapper"></div>
+      </div>
+    </div>
   )
 }
 
