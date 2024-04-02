e81550bdf2ee3645a234ff27133fdd8274ec174b
anyars-encarta <anyarsencarta@gmail.com>
2024-04-02 09:23:09 +0000

    Complete left section of single page

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -3,7 +3,16 @@ import './slider.scss';
 
 const Slider = ({ images }) => {
   return (
-    <div className='slider'>Slider</div>
+    <div className='slider'>
+        <div className="bigImage">
+            <img src={images[0]} alt="" />
+        </div>
+        <div className="smallImages">
+            {images.slice(1).map((image, index) => (
+                <img key={index} src={image} alt='' />
+            ))}
+        </div>
+    </div>
   )
 }
 
